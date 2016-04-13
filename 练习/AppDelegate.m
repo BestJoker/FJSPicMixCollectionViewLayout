7546a419a45d95df23a4bae2421ec2eabf6c69fa
BestJoker <fujinshi@biquu.com>
2016-04-13 23:44:34 +0800

    no message

--- 练习/AppDelegate.m
+++ 练习/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
